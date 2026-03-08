a38703a275d767837970351110bd1a01c736b8fd
Gadiel <quinterogadiel17@gmail.com>
2026-03-08 08:11:44 -0500

    agregar cambios faltantes

--- database/conexion.py
+++ database/conexion.py
@@ -1,11 +1,12 @@
 import mysql.connector
+import os # Importante para que funcione en internet
 
 def obtener_conexion():
-    conexion = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="",
-        database="sistema_votacion"
+    return mysql.connector.connect(
+        # Aquí ponemos los datos que te dio Aiven
+        host=os.getenv('DB_HOST', 'localhost'), 
+        user=os.getenv('DB_USER', 'root'), 
+        password=os.getenv('DB_PASSWORD', ''), 
+        database=os.getenv('DB_NAME', 'sistema_votacion'),
+        port=os.getenv('DB_PORT', 3306)
     )
-    
-    return conexion
